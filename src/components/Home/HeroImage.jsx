d495d688db221d38a26d21be55369f7d6228488e
Ajay Yadav <imajydv8298@gmail.com>
2024-04-04 11:42:21 +0530

    changed the hero image

--- src/components/Home/HeroImage.jsx
+++ src/components/Home/HeroImage.jsx
@@ -1,9 +1,11 @@
 import style from "./HeroImage.module.css";
-import hero from "../../assets/hero.png";
 const HeroImage = () => {
   return (
     <div className={style.img_container}>
-      <img src={hero} alt="Ajay Yadav (ATechAjay)" />
+      <img
+        src="https://res.cloudinary.com/dxowsspd6/image/upload/v1712210835/AJ_Portfolio_Assets/Hero_z0a3ql.webp"
+        alt="Ajay Yadav (ATechAjay)"
+      />
     </div>
   );
 };
