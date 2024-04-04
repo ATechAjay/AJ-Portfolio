1fc0d0cf9146499419264dbca413175988c0a140
Ajay Yadav <imajydv8298@gmail.com>
2024-04-04 11:25:12 +0530

    Fix bug and improve the style of the card and hero image

--- src/components/pages/Project/Project.jsx
+++ src/components/pages/Project/Project.jsx
@@ -3,7 +3,7 @@ import style from "./Project.module.css";
 const Project = () => {
   return (
     <div className={style.cards_container}>
-      <h1>Tip of the iceberg</h1>
+      <h1>Tip of the iceberg </h1>
 
       <div className={style.cards}>
         <ProjectCard />
