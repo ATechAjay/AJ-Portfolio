aebafcb79b60648041f581513094e2209352b901
Ajay Yadav <imajydv8298@gmail.com>
2024-04-15 09:25:37 +0530

    Remove projects list

--- src/components/utils/Data.js
+++ src/components/utils/Data.js
@@ -1,75 +1,19 @@
 const PROJECT_DATA = [
   {
     id: "01",
-    projectName: "Image Filter App",
+    projectName: "AJ Portfolio",
     image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/image_filter_app_ddegl7.webp",
-    techStack: "JavaScript App",
+      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712646827/AJ_Portfolio_Assets/aj-portfolio_cusrle.webp",
+    techStack: "React App",
     description:
-      "Easily enhance photos with filters like blur, grayscale, and vibrant colors. User-friendly interface with real-time previews for quick creative edits.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-filter-image-v2.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/22%20-%20Image%20filter%20app",
+      "I've created my own portfolio website, where I can demonstrate my work professionally or on social media as a content creator.",
+    techTags: ["HTML", "CSS", "JavaScript", "React", "React-Router"],
+    hostedLink: "/",
+    gitHubLink: "https://github.com/ATechAjay/AJ-Portfolio",
   },
 
   {
     id: "02",
-    projectName: "Expense Tracker App",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424356/AJ_Portfolio_Assets/todo_list_app_t1v8ta.webp",
-    techStack: "React App",
-    description:
-      "Track expenses effortlessly. Store items, dates, and amounts in a simple list format. Stay organized and in control of spending with this intuitive app, now with the ability to filter expenses by year.",
-    techTags: ["HTML", "CSS", "JavaScript", "React"],
-    hostedLink: "https://aj-todolist.netlify.app/",
-    gitHubLink: "#",
-  },
-
-  {
-    id: "03",
-    projectName: "Age Calculator App",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/calculator_app_xogq9g.webp",
-    techStack: "JavaScript App",
-    description:
-      "Input your birth date and get your age instantly in various time formats like years, months, days, weeks, hours, minutes, seconds, and milliseconds.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-age-calculator.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/23%20-%20Age%20calculator",
-  },
-
-  {
-    id: "04",
-    projectName: "Live Currency Converter App",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712196486/AJ_Portfolio_Assets/live_currency_h4luw6.webp",
-    techStack: "JavaScript App",
-    description:
-      "A simple currency conversion app that allows users to convert currencies from one denomination to another.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-currency-converter.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/25%20-%20Live%20currency%20converter",
-  },
-
-  {
-    id: "05",
-    projectName: "Box-Shadow Generator App",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/box_shadow_generator_app_mzsq8a.webp",
-    techStack: "JavaScript App",
-    description:
-      "Create and visualize box-shadows effortlessly. Adjust parameters and preview in real-time. Copy generated CSS with a click for easy implementation.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-box-shadow-generator.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/21%20-%20Box%20shadow%20controller",
-  },
-
-  {
-    id: "06",
     projectName: "College Syllabus App",
     image:
       "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/beu_app_bmucpr.webp",
@@ -89,21 +33,62 @@ const PROJECT_DATA = [
   },
 
   {
-    id: "07",
-    projectName: "Choose background",
+    id: "03",
+    projectName: "Expense Tracker App",
     image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/bg_changer_app_agfna2.webp",
-    techStack: "JavaScript App",
+      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424356/AJ_Portfolio_Assets/todo_list_app_t1v8ta.webp",
+    techStack: "React App",
     description:
-      "Instantly change backgrounds with ease. Choose from gradients or images to enhance your projects effortlessly.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-background-changer-v2.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/20%20-%20Background%20choosing%20with%20images%20and%20gradients",
+      "Track expenses effortlessly. Store items, dates, and amounts in a simple list format. Stay organized and in control of spending with this intuitive app, now with the ability to filter expenses by year.",
+    techTags: ["HTML", "CSS", "JavaScript", "React"],
+    hostedLink: "https://aj-todolist.netlify.app/",
+    gitHubLink: "#",
   },
 
   {
-    id: "08",
+    id: "04",
+    projectName: "Live Currency Converter App",
+    image:
+      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712196486/AJ_Portfolio_Assets/live_currency_h4luw6.webp",
+    techStack: "JavaScript App",
+    description:
+      "A simple currency conversion app that allows users to convert currencies from one denomination to another.",
+    techTags: ["HTML", "CSS", "JavaScript"],
+    hostedLink: "https://aj-currency-converter.netlify.app/",
+    gitHubLink:
+      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/25%20-%20Live%20currency%20converter",
+  },
+
+  {
+    id: "05",
+    projectName: "Image Filter App",
+    image:
+      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/image_filter_app_ddegl7.webp",
+    techStack: "JavaScript App",
+    description:
+      "Easily enhance photos with filters like blur, grayscale, and vibrant colors. User-friendly interface with real-time previews for quick creative edits.",
+    techTags: ["HTML", "CSS", "JavaScript"],
+    hostedLink: "https://aj-filter-image-v2.netlify.app/",
+    gitHubLink:
+      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/22%20-%20Image%20filter%20app",
+  },
+
+  {
+    id: "06",
+    projectName: "Age Calculator App",
+    image:
+      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/calculator_app_xogq9g.webp",
+    techStack: "JavaScript App",
+    description:
+      "Input your birth date and get your age instantly in various time formats like years, months, days, weeks, hours, minutes, seconds, and milliseconds.",
+    techTags: ["HTML", "CSS", "JavaScript"],
+    hostedLink: "https://aj-age-calculator.netlify.app/",
+    gitHubLink:
+      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/23%20-%20Age%20calculator",
+  },
+
+  {
+    id: "07",
     projectName: "Firebase Todo App",
     image:
       "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/firebase_app_oivnwq.webp",
@@ -112,31 +97,6 @@ const PROJECT_DATA = [
       "Sync tasks seamlessly across devices using Firebase's real-time database. Enjoy reliable updates and effortless task management for enhanced productivity.",
     techTags: ["HTML", "CSS", "JavaScript", "Firebase"],
     hostedLink: "https://aj-firebase-v1.netlify.app/",
-    gitHubLink: "#",
-  },
-  {
-    id: "09",
-    projectName: "Simple Todo App",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424355/AJ_Portfolio_Assets/notes_taking_app_dmpe9m.webp",
-    techStack: "JavaScript App",
-    description:
-      "Organize tasks effortlessly with this browser-based Todo app, utilizing local storage for seamless access and management.",
-    techTags: ["HTML", "CSS", "JavaScript"],
-    hostedLink: "https://aj-notes-app.netlify.app/",
-    gitHubLink:
-      "https://github.com/ATechAjay/50-The-Ultimate-JavaScript-Projects-Series/tree/main/19%20-%20Notes%20app",
-  },
-  {
-    id: "10",
-    projectName: "Hero Section Design",
-    image:
-      "https://res.cloudinary.com/dxowsspd6/image/upload/v1712424354/AJ_Portfolio_Assets/hero_section_design_a2hpgh.webp",
-    techStack: "HTML/CSS",
-    description:
-      "Recreate stunning Figma designs with precision. Simplify replicating captivating hero sections for your projects.",
-    techTags: ["HTML", "CSS"],
-    hostedLink: "https://atechajay-react-hero-section.netlify.app/",
     gitHubLink: "#",
   },
 ];
