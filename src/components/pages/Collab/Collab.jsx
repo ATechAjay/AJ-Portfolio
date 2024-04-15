638f325e971dd86cb6127049dfda59c8f6b71780
Ajay Yadav <imajydv8298@gmail.com>
2024-04-15 17:06:03 +0530

    fix the para

--- src/components/pages/Collab/Collab.jsx
+++ src/components/pages/Collab/Collab.jsx
@@ -6,10 +6,12 @@ const Collab = () => {
       <h1 className={style.title}>Hi, I&rsquo;m Ajay</h1>
       <p className={style.description}>
         As a frontend developer and AI tool tester, I love helping more than 24K
-        Twitter followers find the right AI tools for their needs. I simplify
-        complex tech topics into easy-to-understand threads and articles,
-        guiding everyone on how to use the latest AI effectively. My goal is to
-        make cutting-edge technology accessible and useful for all.
+        Twitter followers find the right AI tools for their needs.
+        <p className={style.description}>
+          I simplify complex tech topics into easy-to-understand threads and
+          articles, guiding everyone on how to use the latest AI effectively. My
+          goal is to make cutting-edge technology accessible and useful for all.
+        </p>
       </p>
       <h2 className={style.sponser_title}>Sponsorships</h2>
       <div className={style.card_container}>
