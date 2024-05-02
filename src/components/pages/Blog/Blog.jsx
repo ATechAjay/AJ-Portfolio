e46e5ef5dd77cc837e3c3381a390bd4b646e94a9
Ajay Yadav <imajydv8298@gmail.com>
2024-05-02 17:58:09 +0530

    Added the link of the blogs as well a little bit about me.

--- src/components/pages/Blog/Blog.jsx
+++ src/components/pages/Blog/Blog.jsx
@@ -1,12 +1,26 @@
 import style from "./Blog.module.css";
-import BlogCard from "./BlogCard";
+
 const Blog = () => {
   return (
     <div className={style.blog_container}>
       <h1>Technical Blogs</h1>
-      <div className={style.blogs}>
-        <BlogCard />
-      </div>
+      <p className={style.description}>
+        As a Technical Writer at Talent500 – Blogs since June 2022, I&apos;ve
+        authored 12+ engaging blogs on HTML, CSS, JavaScript, React, and
+        Firebase, ensuring clarity and accuracy. Additionally, I manage a
+        Twitter channel focused on front-end technologies, attracting over 24K+
+        followers with detailed threads simplifying complex topics like HTML,
+        CSS, JavaScript, React, Tailwind, and Bootstrap, fostering engagement
+        and understanding.
+      </p>
+
+      <a
+        href="https://talent500.co/blog/author/ajay-yadav/"
+        target="_blank"
+        className={style.link}
+      >
+        Read all the blogs
+      </a>
     </div>
   );
 };
